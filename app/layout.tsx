679e326ef0c491571e028fca40ca42148f6a7759
JeongSeonP <finchblue@naver.com>
2023-08-16 20:53:58 +0900

    :children_crossing: Suspense & loading컴포넌트추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
-// "use client";
 import Header from "@/components/common/Header";
 import "./globals.css";
 import type { Metadata } from "next";
 import Footer from "@/components/common/Footer";
 import RecoilRootWrapper from "@/components/common/RecoilRootWrapper";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       <body>
         <RecoilRootWrapper>
           <Header />
-          {children}
+          <Suspense fallback={<Loading />}>{children}</Suspense>
           <Footer />
         </RecoilRootWrapper>
       </body>
