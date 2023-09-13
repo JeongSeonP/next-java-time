4da283a9722e3e00923db4c93d1660d0ca83321a
JeongSeonP <finchblue@naver.com>
2023-09-13 20:18:54 +0900

    :construction: Web share API test

--- app/stores/[id]/ShareButton.tsx
+++ app/stores/[id]/ShareButton.tsx
@@ -2,23 +2,27 @@ import { usePathname } from "next/navigation";
 import { IoIosShareAlt } from "react-icons/io";
 
 const ShareButton = () => {
+  const pathname = usePathname();
+  const baseUrl = "https://next-java-time.vercel.app";
   const shareData = {
     title: "Java Time",
     text: "커피에 진심이신가요? Java Time에서 커피리뷰를 공유해보세요!",
-    url: window.location.href,
+    url: `${baseUrl}${pathname}`,
   };
-
+  console.log(shareData);
   const handleShare = async () => {
     if (navigator.canShare(shareData)) {
+      console.log("canShare");
       try {
         await navigator.share(shareData);
+        console.log("Share");
       } catch (error) {
         throw new Error(
           `handleShare Error: Time(${new Date()}) ERROR ${error}`
         );
       }
     } else if (navigator.clipboard) {
-      navigator.clipboard.writeText(window.location.href);
+      navigator.clipboard.writeText(shareData.url);
     }
   };
   return (
